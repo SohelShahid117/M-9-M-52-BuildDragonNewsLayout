9a24610b49f2c426e22432787944777b6cf26688
SohelShahid117 <sohelshahid09@gmail.com>
2024-07-07 11:14:21 +0600

    52-9 Left Side Navigation And Module Summary done

--- src/Components/LeftSideNav/LeftSideNav.jsx
+++ src/Components/LeftSideNav/LeftSideNav.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftSideNav = () => {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetch("categories.json")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
   return (
     <div>
-      <h2>Left side nav</h2>
+      <h2>Left side nav:{categories.length}</h2>
+      <h2>All Categories</h2>
+      {categories.map((c) => (
+        <Link key={c.id} className="block my-2 text-2xl" to={`/c/${c.id}`}>
+          {c.name}
+        </Link>
+      ))}
     </div>
   );
 };
--- src/Components/RightSideNav/RightSideNav.jsx
+++ src/Components/RightSideNav/RightSideNav.jsx
@@ -1,9 +1,44 @@
 import React from "react";
+import { FaFacebook, FaTwitter } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { IoLogoGithub } from "react-icons/io";
+import { Link } from "react-router-dom";
+import qZone1 from "../../../DRAGON/dragon-news-resources/assets/qZone1.png";
+import qZone2 from "../../../DRAGON/dragon-news-resources/assets/qZone2.png";
+import qZone3 from "../../../DRAGON/dragon-news-resources/assets/qZone3.png";
 
 const RightSideNav = () => {
   return (
     <div>
       <h2>Right side nav</h2>
+      <div className="mb-6 border">
+        <h2>Login With</h2>
+        <button className="btn btn-outline">
+          <FcGoogle />
+          Google Login
+        </button>
+        <button className="btn btn-outline">
+          <IoLogoGithub />
+          Github Login
+        </button>
+      </div>
+      <div className="mb-6 border">
+        <h2>Find us on</h2>
+        <Link className="btn btn-outline">
+          <FaFacebook />
+          Facebook
+        </Link>
+        <Link className="btn btn-outline">
+          <FaTwitter />
+          Twiteer
+        </Link>
+      </div>
+      <div className="mb-6 border">
+        <h2>Q Zone</h2>
+        <img src={qZone1}></img>
+        <img src={qZone2}></img>
+        <img src={qZone3}></img>
+      </div>
     </div>
   );
 };
